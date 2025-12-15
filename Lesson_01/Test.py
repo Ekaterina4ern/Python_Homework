d4d35748bf8cc7d660ab2e3f6dd400f77de4deec
Ekaterina <kcdoc1993@gmail.com>
2025-12-16 01:30:45 +0500

    Урок 1

--- Lesson_01/Test.py
+++ Lesson_01/Test.py
@@ -1,2 +1,11 @@
-def sumNumbers(a, b): # на этой строке объявление функции, ниже - тело
-    print(a+b)
+def print_letter(let):
+    print(let, end='')
+
+
+print_letter('C')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
