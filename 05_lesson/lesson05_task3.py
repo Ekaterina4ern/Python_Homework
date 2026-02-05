69dbf71a6bd3c8ba5d2b7005e2d9de6ba40c64d4
Ekaterina <kcdoc1993@gmail.com>
2026-02-05 16:43:10 +0500

    Урок 5

---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,22 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox(service=FirefoxService(executable_path=GeckoDriverManager().install()), )
+
+driver.maximize_window()
+
+driver.get("http://the-internet.herokuapp.com/inputs")
+
+input_field = driver.find_element(By.TAG_NAME, "input")
+input_field.send_keys("Sky")
+
+sleep(3)
+input_field.clear()
+
+sleep(1)
+input_field.send_keys("Pro")
+sleep(10)
